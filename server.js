f61fc08b38cfb32ad8bd537a36658925644531ee
smailnajim <smailnajim2003@gmail.com>
2025-10-09 22:54:53 +0200

    create apliction, define engine view, use middelware to parce http post, conect with db

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 require('dotenv').config();
 
-console.log('----\n', process.env.A);
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+mongoose.connect(process.env.URL_TO_CONNECT)
+.then(()=>console.log('\nconsole\n'))
+.catch(error => console.log('\nthere is error\n', error));
