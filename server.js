6e71dd8e40216b431e099f5dfb6d69d4edf5bd01
smailnajim <smailnajim2003@gmail.com>
2025-10-11 18:54:38 +0200

    .

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-require('dotenv').config();
+    require('dotenv').config();
+    const express = require('express');
+    const path = require('path');
+    const mongoose = require('mongoose');
 
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
 
-const app = express();
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'));
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+    const app = express();
 
-mongoose.connect(process.env.URL_TO_CONNECT)
-.then(()=>console.log('\nconsole\n'))
-.catch(error => console.log('\nthere is error\n', error));
+
+
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
+
+
+
+    app.use(express.static('public'));
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
+
+
+
+    mongoose.connect(process.env.URL_TO_CONNECT)
+    .then(()=>console.log('\nconsole\n'))
+    .catch(error => console.log('\nthere is error\n', error));
+
+
+
+    app.get('/', (req, res)=>res.send('all greet'));
+
+
+
+    app.listen(3000, ()=>console.log('app work on http://localhost:3000'));
