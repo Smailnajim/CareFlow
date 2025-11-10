0afd753efdde147a8eb6128eb54284a5edf5b5b8
smailnajim <smailnajim2003@gmail.com>
2025-11-10 13:49:09 +0100

    not

--- Enum/Permissions.js
+++ Enum/Permissions.js
@@ -1,27 +1,27 @@
 module.exports = {
-    // User management
+    
     CREATE_USER: 'create_user',
     UPDATE_USER: 'update_user',
     DELETE_USER: 'delete_user',
     VIEW_USER: 'view_user',
     
-    // Rendezvous management
+    
     CREATE_RENDEZVOUS: 'create_rendezvous',
     UPDATE_RENDEZVOUS: 'update_rendezvous',
     DELETE_RENDEZVOUS: 'delete_rendezvous',
     VIEW_RENDEZVOUS: 'view_rendezvous',
     VIEW_ALL_RENDEZVOUS: 'view_all_rendezvous',
     
-    // Prescription management
+    
     CREATE_PRESCRIPTION: 'create_prescription',
     UPDATE_PRESCRIPTION: 'update_prescription',
     DELETE_PRESCRIPTION: 'delete_prescription',
     VIEW_PRESCRIPTION: 'view_prescription',
     
-    // Treatment management
+    
     CREATE_TREATMENT: 'create_treatment',
     VIEW_TREATMENT: 'view_treatment',
     
-    // System management
+    
     INIT_ROLES: 'init_roles',
 };
